6d3e0ee6158729bbb7f80130730d59960c418fbb
Aurélien Labate <aurelien@labate.me>
2017-05-03 22:27:44 +0200

    Try to deploy to dokku

---
+++ config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const url = require('url');
+
+var mongo = url.parse(process.env.MONGO_URL);
+
+module.exports = {
+  mongodb: {
+    server: mongo.hostname,
+    port:   mongo.port,
+    ssl: false,
+    autoReconnect: true,
+    poolSize: 4,
+    admin: false,
+    auth: [
+      {
+        database: mongo.pathname.substring(1),
+        username: mongo.auth.split(':')[0],
+        password: mongo.auth.split(':')[1],
+      },
+    ],
+
+    adminUsername: mongo.username,
+    adminPassword: mongo.password,
+  },
+
+  site: {
+    baseUrl: '/',
+    host: '0.0.0.0',
+    port: '8080',
+  },
+
+  useBasicAuth: false,
+  basicAuth: {
+    username: '',
+    password: '',
+  },
+
+};
